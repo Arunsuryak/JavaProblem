36548283f842776ad898872f73d633e5d9be4f3a
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-13 11:26:01 +0530

    added

---
+++ ArithmaticOperations .java
@@ -0,0 +1,12 @@
+package java_programming_basics;
+
+public class ArithmaticOperations {
+     public static void main(String args[]) {
+    	 int a=2 , b=5;
+    	 System.out.println("add:"+(a+b));
+    	 System.out.println("sub:"+(a-b));
+    	 System.out.println("mul:"+(a*b));
+    	 System.out.println("div:"+((float)a/b));
+    	 System.out.println("mod:"+(a%b));
+     }
+} 
