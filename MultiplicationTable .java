e18e1e7cd5c5cec28c80dc4aa027dc8a5ce923af
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-15 07:17:13 +0530

    added

---
+++ MultiplicationTable .java
@@ -0,0 +1,14 @@
+package control_statements;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter number :");
+		int a= in.nextInt();
+		System.out.println("Enter limit :");
+		int b= in.nextInt();		  
+		  }
+	}
