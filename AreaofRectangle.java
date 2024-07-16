d7928b77993dc004590aae4957dcf81538c7581a
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-16 09:13:59 +0530

    add

---
+++ AreaofRectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AreaofRectangle {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Eneter the height");
+		double height=in.nextDouble();
+		System.out.println("Eneter the width");
+		double width=in.nextDouble();
+		System.out.println("Area of rectangle is:"+height*width);
+
+	}
+
+}
