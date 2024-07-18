99490d63f8ef02f4e45d31ca06d6c0e05c9193ee
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-18 20:43:36 +0530

    add

--- PerimeterRectangle.java
+++ PerimeterRectangle.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class PerimeterRectangle {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the height and width");
+	    double a=in.nextDouble();
+	    Double b=in.nextDouble();
+	    System.out.println("perimeter of rectangle is:"+2*(a+b));
+
+	}
+
+}
 
