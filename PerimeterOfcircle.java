5e6fa981e7b7c2c2bf05752d487379dbd3700db9
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-18 20:00:38 +0530

    add

---
+++ PerimeterOfcircle.java
@@ -0,0 +1,14 @@
+mport java.util.Scanner;
+
+public class PerimeterOfcircle {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the radious");
+		double a=in.nextDouble();
+		System.out.println("perimeteris:"+2*Math.PI*a);
+		
+
+	}
+
+}
