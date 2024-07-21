337def50713b8be47c3a6321d265d925da3691b0
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-21 09:47:24 +0530

    add

---
+++ Functionwithparameters.java
@@ -0,0 +1,15 @@
+public class Parameters {
+	  public void sum(int num1,int num2)
+	    {
+	        System.out.println(num1+num2);
+	        
+	    }
+	   public static void main(String[] args) 
+	    {
+	        Parameters obj1 = new Parameters();
+	         obj1.sum(1,2);
+	    }
+	
+}   
+	
+	
