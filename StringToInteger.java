83367ca5bda2e26c75cced43fdf302afb3fae882
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-24 13:46:37 +0530

    add

---
+++ StringToInteger.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String a="10";
+		int b=Integer.parseInt(a);
+		System.out.print(b+2);
+		
+
+	}
+
+}
