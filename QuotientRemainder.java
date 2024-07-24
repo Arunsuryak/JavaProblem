59acc1697cce6e33f902c42b8526b9d7555559ba
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-24 13:44:51 +0530

    add

---
+++ QuotientRemainder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		  Scanner in= new Scanner(System.in);
+		  System.out.println("Enter a value");
+		  int a=in.nextInt();
+		  System.out.println("enter 2 value");
+		  int b=in.nextInt();
+		  System.out.println("quotient is:"+(a/b)+" remainder  is:"+(a%b));
+
+	}
+
+}
