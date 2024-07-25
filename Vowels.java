581eb9cc956e84a8ac5ba62130ceee6308f22372
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-25 20:25:58 +0530

    add

---
+++ Vowels.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		 Scanner in =new Scanner(System.in);
+			char r =in.next().charAt(0);
+			switch(r){
+			case'a':
+				System.out.println("vowels");
+				break;
+			case'e':
+				System.out.println("vowels");
+				break;
+			case'i':
+				System.out.println("vowels");
+				break;
+			case'o':
+				System.out.println("vowels");
+				break;
+			case'u':
+				System.out.println("vowels");
+				break;
+			case'A':
+				System.out.println("vowels");
+				break;
+			case'E':
+				System.out.println("vowels");
+				break;
+			case'I':
+				System.out.println("vowels");
+				break;
+			case'O':
+				System.out.println("vowels");
+				break;
+			case'U':
+				System.out.println("vowels");
+				break;
+			default:
+				System.out.println("not vowels");
+				
+							
+				}
+
+	}
+
+}
