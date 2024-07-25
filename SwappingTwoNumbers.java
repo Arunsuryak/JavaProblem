5656ca8845ff40403d2a31553616d07d27d3d516
Arunsuryak <karunsuryaccb@gmail.com>
2024-07-25 20:01:07 +0530

    add

---
+++ SwappingTwoNumbers.java
@@ -0,0 +1,16 @@
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+	      int a= 5;
+	      int b=8;
+	   int   temp = a;
+	   a =  b;
+	   b= temp;
+	   System.out.println("a Storing:"+a);
+	   System.out.println("b Storing:"+b);
+	   
+	      
+	}
+
+}
